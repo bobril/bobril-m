40a97ea22e71d06928c5342419a4ffbf42b6f3c4
jirgl <jirgl.jakub@gmail.com>
2017-06-05 19:16:16 +0200

    Add clickAwayListener component

---
+++ src/clickAwayListener.ts
@@ -0,0 +1,52 @@
+import * as b from 'bobril';
+
+let layerId: string;
+let onClicks: { [id: string]: () => void } = {};
+
+export interface IClickAwayListenerLayerData { }
+
+interface IClickAwayListenerLayerCtx extends b.IBobrilCtx {
+    data: IClickAwayListenerData;
+}
+
+const clickAwayListenerLayer = b.createComponent<IClickAwayListenerLayerData>({
+    render(ctx: IClickAwayListenerLayerCtx, me: b.IBobrilNode) {
+        me.children = b.styledDiv(undefined, {
+            position: 'absolute',
+            top: 0,
+            width: '100%',
+            height: '100%'
+        });
+    },
+    onPointerUp(ctx: IClickAwayListenerLayerCtx, event: b.IBobrilPointerEvent): boolean {
+        for (var id in onClicks)
+            onClicks[id]();
+
+        return false;
+    }
+});
+
+export interface IClickAwayListenerData {
+    id: string;
+    onClick: () => void;
+}
+
+interface IClickAwayListenerCtx extends b.IBobrilCtx {
+    data: IClickAwayListenerData;
+}
+
+export const ClickAwayListener = b.createComponent<IClickAwayListenerData>({
+    init(ctx: IClickAwayListenerCtx) {
+        const d = ctx.data;
+        onClicks[d.id] = d.onClick;
+        if (!layerId)
+            layerId = b.addRoot(() => clickAwayListenerLayer({ onClick: d.onClick }));
+    },
+    destroy(ctx: IClickAwayListenerCtx) {
+        delete onClicks[ctx.data.id];
+        if (layerId && Object.keys(onClicks).length === 0) {
+            b.removeRoot(layerId);
+            layerId = undefined;
+        }
+    },
+});
