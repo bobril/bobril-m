5105cb7edb8a1bcd41be6b7e7b683030f7c9fc6d
Boris Letocha <Boris.Letocha@gmail.com>
2019-06-05 02:21:46 +0200

    Update dependencies and prettier.

--- src/enhancedTextArea.ts
+++ src/enhancedTextArea.ts
@@ -4,32 +4,38 @@ import * as c from "./styleConsts";
 
 const rowsHeight = 24;
 
-var textAreaStyle = b.styleDef({
-    width: '100%',
-    resize: 'none',
-    font: 'inherit',
-    color: 'inherit',
-    backgroundColor: colors.transparent,
-    padding: 0,
-    border: 0
-}, {
+var textAreaStyle = b.styleDef(
+    {
+        width: "100%",
+        resize: "none",
+        font: "inherit",
+        color: "inherit",
+        backgroundColor: colors.transparent,
+        padding: 0,
+        border: 0
+    },
+    {
         focus: {
             outline: 0
         }
-    });
+    }
+);
 
-var textAreaShadowStyle = b.styleDef([c.positionAbsolute, {
-    width: '100%',
-    resize: 'none',
-    // Overflow also needed to here to remove the extra row
-    // added to textareas in Firefox.
-    overflow: 'hidden',
-    // Visibility needed to hide the extra text area on ipads
-    visibility: 'hidden',
-    font: 'inherit',
-    padding: 0,
-    border: 0
-}]);
+var textAreaShadowStyle = b.styleDef([
+    c.positionAbsolute,
+    {
+        width: "100%",
+        resize: "none",
+        // Overflow also needed to here to remove the extra row
+        // added to textareas in Firefox.
+        overflow: "hidden",
+        // Visibility needed to hide the extra text area on ipads
+        visibility: "hidden",
+        font: "inherit",
+        padding: 0,
+        border: 0
+    }
+]);
 
 export interface IEnhancedTextAreaData extends b.IValueData<string> {
     disabled?: boolean;
@@ -47,7 +53,9 @@ interface IEnhancedTextAreaCtx extends b.IBobrilCtx {
 }
 
 function syncShadow(ctx: IEnhancedTextAreaCtx, newValue?: string) {
-    const shadowEl = <HTMLTextAreaElement>(<b.IBobrilCacheNode[]>ctx.me.children)[0].element;
+    const shadowEl = <HTMLTextAreaElement>(
+        (<b.IBobrilCacheNode[]>ctx.me.children)[0].element
+    );
     const d = ctx.data;
     if (newValue !== undefined) {
         shadowEl.value = newValue;
@@ -92,12 +100,28 @@ export const EnhancedTextarea = b.createComponent<IEnhancedTextAreaData>({
         b.style(me, d.style);
         const rowStr = "" + (d.rows || 1);
         me.children = [
-            b.style({ tag: "textarea", attrs: { readOnly: true, tabindex: -1, rows: rowStr } }, textAreaShadowStyle),
-            b.style({
-                tag: "textarea",
-                attrs: { disabled: d.disabled, tabindex: d.disabled ? -1 : (d.tabindex || 0), value: b.getValue(d.value), rows: rowStr, id: d.id },
-                component: realTextAreaComponent
-            }, textAreaStyle, { height: ctx.height })
+            b.style(
+                {
+                    tag: "textarea",
+                    attrs: { readOnly: true, tabindex: -1, rows: rowStr }
+                },
+                textAreaShadowStyle
+            ),
+            b.style(
+                {
+                    tag: "textarea",
+                    attrs: {
+                        disabled: d.disabled,
+                        tabindex: d.disabled ? -1 : d.tabindex || 0,
+                        value: b.getValue(d.value),
+                        rows: rowStr,
+                        id: d.id
+                    },
+                    component: realTextAreaComponent
+                },
+                textAreaStyle,
+                { height: ctx.height }
+            )
         ];
     }
 });
